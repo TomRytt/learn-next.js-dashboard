4d5adc6c70b7c67678fca4aa04f65e92628a455f
Tom_Rytt <tom.ofekrytt@elbitsystems.com>
2024-02-26 17:24:06 +0200

    chapter5- Navigating Between Pages

--- nextjs-dashboard/prettier.config.js
+++ nextjs-dashboard/prettier.config.js
@@ -2,5 +2,9 @@ const styleguide = require('@vercel/style-guide/prettier');
 
 module.exports = {
   ...styleguide,
-  plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+  plugins: [
+    ...styleguide.plugins,
+    'prettier-plugin-tailwindcss',
+    'prettier-plugin-classnames',
+  ],
 };
